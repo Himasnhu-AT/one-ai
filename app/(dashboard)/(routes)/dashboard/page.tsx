9754bbac322bc2a2ab23a4304060dbe3cadc7c1e
Himanshu-AT <aristech.official@gmail.com>
2023-11-16 16:16:52 +0530

    milestone 1: sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs';
 
 const DashBoardPage = () => {
     return (
         <div>
             DashBoard Page (Protected);
-            <UserButton afterSignOutUrl='/' />
         </div>
     )
 }
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "./sidebar";
+
+const MobileSideBar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side={"left"} className="p-0">
+                <SideBar />
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileSideBar;
